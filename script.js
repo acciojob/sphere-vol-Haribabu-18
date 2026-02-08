38c75cc224ad3c21d51773a666487a296e8cfb09
HARI BABU ANAKAPALLI <142531911+Haribabu-18@users.noreply.github.com>
2026-02-08 11:39:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    var r = document.getElementById("radius").value;
+    r = parseFloat(r);
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    var v = (r >= 0 && !isNaN(r)) ? ((4 / 3) * Math.PI * r * r * r).toFixed(4) : NaN;
+
+    document.getElementById("volume").value = v;
+    return false;
+}
+
+window.onload = function () {
+    document.getElementById("MyForm").onsubmit = volume_sphere;
+};
